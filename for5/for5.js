180ecfd8570547b55ca4d3e51eee0cf872b09606
federicojoaquincosta <93017131+federicojoaquincosta@users.noreply.github.com>
2022-05-13 12:06:12 -0300

    for5 con if

--- for5/for5.js
+++ for5/for5.js
@@ -1,12 +1,25 @@
 let numeroIteraciones = 0;
+/*if (numeroIteraciones == 0) {
+ //   document.querySelector("#meses_sub").innerHTML = `
+//<option value="0">Seleccione</option>` }
+*/
 
 function generarOpciones() {
     numeroIteraciones = document.querySelector("#numero_opciones").value
+    document.querySelector("#meses_sub").innerHTML = `
+    <option value="0">Seleccione</option>`
 
     for (let i = 1; i <= numeroIteraciones; i++) {
-        document.querySelector("#meses_sub").innerHTML += `
-        <option value="${i}">${i} meses</option>
-    <br>
-    `
+
+        if (i == 1) {
+            document.querySelector("#meses_sub").innerHTML += `
+                <option value="${i}">${i} mes</option> `
+        } else {
+            document.querySelector("#meses_sub").innerHTML += `
+            <option value="${i}">${i} meses</option>
+            <br>
+            `
+        }
     }
 }
+
